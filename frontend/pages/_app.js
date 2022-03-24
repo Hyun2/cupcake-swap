99fa91a3c107532a5d87c8166817e15e104a10ed
hyun2 <hks0284@gmail.com>
2022-03-24 19:52:57 +0900

    feat: 메타마스크 지갑 연결 추가

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,34 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
 import Layout from "../components/Layout";
 import "../styles/global.css";
+import Web3 from "web3";
+import { useStore } from "../utils/store";
+import { connectMetamask } from "../utils";
 
 export default function App(props) {
   const { Component, pageProps } = props;
+  const setWeb3 = useStore((state) => state.setWeb3);
+  const setWalletAddress = useStore((state) => state.setWalletAddress);
+
+  useEffect(() => {
+    console.log("first");
+    if (typeof window.ethereum !== "undefined") {
+      // window.ethereum이 있다면
+      try {
+        const web = new Web3(window.ethereum); // 새로운 web3 객체를 만든다
+        setWeb3(web);
+
+        window.ethereum.on("accountsChanged", () => {
+          connectMetamask({ setWalletAddress });
+        });
+        // return window.ethereum.removeListener("accountsChanged", () => {});
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [setWeb3, setWalletAddress]);
 
   return (
     <>
---
+++ frontend/utils/store.js
@@ -0,0 +1,9 @@
+import create from "zustand";
+
+export const useStore = create((set) => ({
+  web3: null,
+  setWeb3: (web3) => set({ web3 }),
+
+  walletAddress: null,
+  setWalletAddress: (walletAddress) => set({ walletAddress }),
+}));
