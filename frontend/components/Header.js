e7decd6834aa686882c60379bb97b15ba22418b5
hyun2 <hks0284@gmail.com>
2022-03-24 15:12:06 +0900

    feat: 프론트엔드 헤더, 네브바, 컨텐츠 영역 레이아웃 구성

--- frontend/components/Header.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function Header() {
-  return (
-    <div>
-      헤더
-    </div>
-  )
-}
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import Layout from "../components/Layout";
+import "../styles/global.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App(props) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Cupcake Swap</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        {/* <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet" /> */}
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
