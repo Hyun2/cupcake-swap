d473227d7e1bb2eb324cadc40dcd7cd8a9c5a7c3
hyun2 <hks0284@gmail.com>
2022-03-28 13:31:42 +0900

    feat: 디자인 참고(스왑키위) 추가 및 카드 컴포넌트 구현

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,13 @@
+import AddressInput from "../components/AddressInput";
+import NftCard from "../components/NftCard";
+import ThumbnailCard from "../components/ThumbnailCard";
+
 export default function Home() {
-  return <div>your nfts</div>;
+  return (
+    <div>
+      <ThumbnailCard src="https://openseauserdata.com/files/25a27e7885076582601e26d9f1d4296b.svg" />
+      <NftCard src="https://openseauserdata.com/files/25a27e7885076582601e26d9f1d4296b.svg" />
+      <AddressInput />
+    </div>
+  );
 }
