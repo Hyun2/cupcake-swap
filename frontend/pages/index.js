b963ace6dbc725389feac7e658ec0c0f5a596d8a
hyun2 <hks0284@gmail.com>
2022-03-28 16:44:28 +0900

    feat: NftCard 컴포넌트 수정

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,44 @@
+import axios from "axios";
+import { useState, useEffect, useCallback } from "react";
 import AddressInput from "../components/AddressInput";
 import NftCard from "../components/NftCard";
+import NotConnected from "../components/NotConnected";
 import ThumbnailCard from "../components/ThumbnailCard";
+import { useStore } from "../utils/store";
 
 export default function Home() {
+  const walletAddress = useStore((state) => state.walletAddress);
+
+  return walletAddress ? <Connected /> : <NotConnected />;
+}
+
+const Connected = () => {
+  const [nfts, setNfts] = useState([]);
+  const walletAddress = useStore((state) => state.walletAddress);
+
+  const getNfts = useCallback(async (walletAddress) => {
+    const {
+      data: { assets },
+    } = await axios.get(`https://api.opensea.io/api/v1/assets?owner=${walletAddress}`);
+    console.log(assets);
+    setNfts(assets);
+  }, []);
+
+  useEffect(() => {
+    //https://api.opensea.io/api/v1/assets?owner=${address}
+    walletAddress && getNfts(walletAddress);
+  }, [getNfts, walletAddress]);
+
   return (
     <div>
       <ThumbnailCard src="https://openseauserdata.com/files/25a27e7885076582601e26d9f1d4296b.svg" />
-      <NftCard src="https://openseauserdata.com/files/25a27e7885076582601e26d9f1d4296b.svg" />
+      {nfts.length > 0 &&
+        nfts.map((nft, idx) => {
+          return nft.image_url ? (
+            <NftCard key={idx} name={nft.name} collectionName={nft.collection?.name} src={nft.image_url} />
+          ) : null;
+        })}
       <AddressInput />
     </div>
   );
-}
+};
