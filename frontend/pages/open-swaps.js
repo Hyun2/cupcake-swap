1b11bb1b8a3474f48e86315a55a06e7e844946b7
kgd7054 <kgd7054@gmail.com>
2022-03-29 13:56:58 +0900

    openswap page 작성중

--- frontend/pages/open-swaps.js
+++ frontend/pages/open-swaps.js
@@ -1,5 +1,39 @@
+import axios from "axios";
+import Pannel from "../components/Pannel";
+import { useState, useEffect, useCallback } from "react";
+import { useStore } from "../utils/store";
+import NftSwapCard from "../components/NftCard";
+
 const OpenSwaps = () => {
-  return <div>open swaps</div>;
+  // return <div>open swaps</div>;
+  const [nfts, setNfts] = useState([]);
+  const [targetNfts, setTargetNfts] = useState([]);
+  const walletAddress = useStore((state) => state.walletAddress);
+
+  const getNfts = useCallback(async (walletAddress) => {
+    const {
+      data: { assets },
+    } = await axios.get(`https://testnets-api.opensea.io/api/v1/assets?owner=${walletAddress}`);
+    console.log(assets);
+    setNfts(assets);
+  }, []);
+
+  useEffect(() => {
+    //https://api.opensea.io/api/v1/assets?owner=${address}
+    walletAddress && getNfts(walletAddress);
+  }, [getNfts, walletAddress]);
+  return (
+    <>
+      <Pannel>
+        {nfts.length > 0 &&
+          nfts.map((nft, idx) => {
+            return nft.image_url ? (
+              <NftSwapCard key={idx} name={nft.name} collectionName={nft.collection?.name} src={nft.image_url} />
+            ) : null;
+          })}
+      </Pannel>
+    </>
+  );
 };
 
 export default OpenSwaps;
